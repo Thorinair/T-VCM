2e3ea054aabcb5a24235c144c30ad3326ede8815
Thorinair <thorinair@yahoo.com>
2022-08-14 15:56:39 +0200

    Add display.

--- T-VCM.ino
+++ T-VCM.ino
@@ -14,11 +14,14 @@
  ***************************************************/
 
 #include "INA219-SOLDERED.h"
+#include "OLED-Display-SOLDERED.h"
 
-#include "INA219Helper.h"
+#include "splash.h"
 
 INA219 inaP;
 INA219 inaN;
+
+OLED_Display display;
 
 float voltageP = 0;
 float voltageN = 0;
@@ -26,65 +29,79 @@ float currentP = 0;
 float currentN = 0;
 
 void setupIna219() {
-    Serial.println("Initialize INA219");
-    Serial.println("-----------------------------------------------");
-
-    // Default INA219 address is 0x40
     inaP.begin(0x40);
     inaN.begin(0x44);
 
-    // Configure INA219
     inaP.configure(INA219_RANGE_16V, INA219_GAIN_80MV, INA219_BUS_RES_12BIT, INA219_SHUNT_RES_12BIT_16S);
     inaN.configure(INA219_RANGE_16V, INA219_GAIN_80MV, INA219_BUS_RES_12BIT, INA219_SHUNT_RES_12BIT_16S);
 
     // Calibrate INA219. Rshunt = 0.1 ohm, Max excepted current = 800 mA
     inaP.calibrate(0.1, 0.8);
     inaN.calibrate(0.1, 0.8);
+}
 
-    // Display configuration
-    Serial.println("+12V Rail:");
-    checkConfig(inaP);
-    Serial.println("");
-    Serial.println("-12V Rail:");
-    checkConfig(inaN);
+void setupDisplay() {  
+    if (!display.begin()) {
+        Serial.println("Display init failed!");
+        for (;;)
+            ;
+    }
 
-    Serial.println("-----------------------------------------------");
+    display.clearDisplay();
+    drawLogo();
+    display.display();
+
+    delay(2000);
+
+    display.clearDisplay();
+    display.display();
 }
 
 void setup() {
     Serial.begin(115200);
     
     setupIna219();
+    setupDisplay();
 }
 
-void processMeasure() {
-    voltageP = inaP.readBusVoltage();
-    currentP = abs(inaP.readShuntCurrent() * 1000);
-
-    voltageN = inaN.readBusVoltage();
-    currentN = abs(inaN.readShuntCurrent() * 1000);
-}
+//void processMeasure() {
+//    voltageP = inaP.readBusVoltage();
+//    currentP = abs(inaP.readShuntCurrent() * 1000);
+//
+//    voltageN = inaN.readBusVoltage();
+//    currentN = abs(inaN.readShuntCurrent() * 1000);
+//}
 
 // Read values in loop
 void loop() {
-    processMeasure();
+//    processMeasure();
+//
+//    Serial.println("+12V Rail:");
+//    Serial.print("  Voltage: ");
+//    Serial.print(voltageP, 2);
+//    Serial.println(" V");
+//    Serial.print("  Current: ");
+//    Serial.print(currentP, 1);
+//    Serial.println(" mA");
+//
+//    Serial.println("-12V Rail:");
+//    Serial.print("  Voltage: ");
+//    Serial.print(voltageN, 2);
+//    Serial.println(" V");
+//    Serial.print("  Current: ");
+//    Serial.print(currentN, 1);
+//    Serial.println(" mA");
+//
+//    Serial.println("");
+//    delay(1000);
+}
 
-    Serial.println("+12V Rail:");
-    Serial.print("  Voltage: ");
-    Serial.print(voltageP, 2);
-    Serial.println(" V");
-    Serial.print("  Current: ");
-    Serial.print(currentP, 1);
-    Serial.println(" mA");
+void drawLogo(void)
+{
+    display.clearDisplay();
 
-    Serial.println("-12V Rail:");
-    Serial.print("  Voltage: ");
-    Serial.print(voltageN, 2);
-    Serial.println(" V");
-    Serial.print("  Current: ");
-    Serial.print(currentN, 1);
-    Serial.println(" mA");
-
-    Serial.println("");
+    display.drawBitmap((display.width() - LOGO_WIDTH) / 2, (display.height() - LOGO_HEIGHT) / 2, logo_bmp, LOGO_WIDTH,
+                       LOGO_HEIGHT, 1);
+    display.display();
     delay(1000);
 }
